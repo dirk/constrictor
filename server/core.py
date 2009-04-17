48c01a26316ddbea928d47327db89506596a505d
Dirk Gadsden <dirk@esherido.com>
2009-04-17 17:08:52 -0400

    Removed unnecessary utils

--- server/core.py
+++ server/core.py
@@ -1,22 +1,47 @@
-import socket
-from utils import *
+from BaseHTTPServer import BaseHTTPRequestHandler
+import urlparse
 
-host = ''
-port = 8080
+class GetHandler(BaseHTTPRequestHandler):
+    def do_GET(self):
+      self.handle_request()
+    def do_POST(self):
+      self.handle_request()
+    def do_PUT(self):
+      self.handle_request()
+    def do_DELETE(self):
+      self.handle_request()
+    def do_HEAD(self):
+      self.handle_request()
+    
+    
+    def handle_request(self):
+        parsed_path = urlparse.urlparse(self.path)
+        """message = '\n'.join([
+                'CLIENT VALUES:',
+                'client_address=%s (%s)' % (self.client_address,
+                                            self.address_string()),
+                'command=%s' % self.command,
+                'path=%s' % self.path,
+                'real path=%s' % parsed_path.path,
+                'query=%s' % parsed_path.query,
+                'request_version=%s' % self.request_version,
+                '',
+                'SERVER VALUES:',
+                'server_version=%s' % self.server_version,
+                'sys_version=%s' % self.sys_version,
+                'protocol_version=%s' % self.protocol_version,
+                '',
+                ])"""
+        self.send_response(200)
+        self.end_headers()
+        #self.wfile.write(message)
+        variables = self.rfile.read(int(self.headers.get('content-length', 0)))
+        request_parts = self.requestline.split(' ')
+        print request_parts
+        return
 
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-# Allow it to reuse (1) addresses (for quicker debugging).
-s.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
-# Bind to the host and port.
-s.bind((host, port))
-s.listen(1)
-while 1:
-  # Accept a new connection
-  sock, addr = s.accept()
-  print "Connected"
-  # Recieve data with a 0 second timeout.
-  data = recv_timeout(sock, 0)
-  print data
-  sock.sendall("test")
-  sock.close()
-s.close()
+if __name__ == '__main__':
+    from BaseHTTPServer import HTTPServer
+    server = HTTPServer(('localhost', 8080), GetHandler)
+    print 'Starting server, use <Ctrl-C> to stop'
+    server.serve_forever()
